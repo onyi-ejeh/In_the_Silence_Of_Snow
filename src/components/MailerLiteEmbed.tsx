b1df5870090cafe76978d86d2150f7745f6e125f
Onyi <vonyiejeh@yahoo.com>
2026-01-06 19:32:56 +0100

    Update MailerLiteEmbed.tsx

--- src/components/MailerLiteEmbed.tsx
+++ src/components/MailerLiteEmbed.tsx
@@ -4,12 +4,31 @@ type MailerLiteEmbedProps = {
   formId: string; // e.g. "Mf34HB"
 };
 
+declare global {
+  interface Window {
+    ml?: (...args: any[]) => void;
+  }
+}
+
 export function MailerLiteEmbed({ formId }: MailerLiteEmbedProps) {
   useEffect(() => {
-    // Ask MailerLite to re-scan the DOM (safe to call multiple times)
-    // @ts-ignore
-    window.ml?.("reinitialize");
-  }, []);
+    let tries = 0;
+
+    const timer = window.setInterval(() => {
+      if (window.ml) {
+        window.ml("reinitialize");
+        window.clearInterval(timer);
+      }
+
+      tries += 1;
+      if (tries > 20) {
+        // stop after ~5 seconds
+        window.clearInterval(timer);
+      }
+    }, 250);
+
+    return () => window.clearInterval(timer);
+  }, [formId]);
 
   return <div className="ml-embedded" data-form={formId} />;
 }
