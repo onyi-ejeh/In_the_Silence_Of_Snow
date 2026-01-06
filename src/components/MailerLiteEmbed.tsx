e01ed117ce77fa766aa67e2d7ce7230c7c5c1f37
Onyi <vonyiejeh@yahoo.com>
2026-01-06 20:23:19 +0100

    Update MailerLiteEmbed.tsx

--- src/components/MailerLiteEmbed.tsx
+++ src/components/MailerLiteEmbed.tsx
@@ -1,7 +1,17 @@
+import { useEffect } from "react";
+
 type MailerLiteEmbedProps = {
   formId: string;
+  accountId?: string;
 };
 
-export function MailerLiteEmbed({ formId }: MailerLiteEmbedProps) {
+export function MailerLiteEmbed({ formId, accountId = "2019276" }: MailerLiteEmbedProps) {
+  useEffect(() => {
+    // Trigger ML to scan again by re-setting the account after the div exists
+    // (This is supported and you already confirmed it returns "2019276")
+    // @ts-ignore
+    window.ml?.("account", accountId);
+  }, [accountId]);
+
   return <div className="ml-embedded" data-form={formId} />;
 }
