2f0858cdfbcc6e4e3c1bb8a86b47bbf143dd1214
Onyi <vonyiejeh@yahoo.com>
2026-01-06 15:09:58 +0100

    Delete src/components/MailerLiteEmbed.tsx

--- src/components/MailerLiteEmbed.tsx
+++
@@ -1,7 +0,0 @@
-type MailerLiteEmbedProps = {
-  formId: string; // e.g. "Mf34HB"
-};
-
-export function MailerLiteEmbed({ formId }: MailerLiteEmbedProps) {
-  return <div className="ml-embedded" data-form={formId} />;
-}
