5cc75c05f7446d684e6d7ff41c736e1b80e6ccd2
Onyi <vonyiejeh@yahoo.com>
2026-01-06 15:58:37 +0100

    Update mailliteembedded.tsx

--- src/components/mailliteembedded.tsx
+++ src/components/mailliteembedded.tsx
@@ -1,1 +1,16 @@
+import { useEffect } from "react";
+
+type MailerLiteEmbedProps = {
+  formId: string; // e.g. "Mf34HB"
+};
+
+export function MailerLiteEmbed({ formId }: MailerLiteEmbedProps) {
+  useEffect(() => {
+    // Ask MailerLite to re-scan the DOM (safe to call multiple times)
+    // @ts-ignore
+    window.ml?.("reinitialize");
+  }, []);
+
+  return <div className="ml-embedded" data-form={formId} />;
+}
 
